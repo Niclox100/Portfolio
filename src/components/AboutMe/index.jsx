b762afa40236d0ce8e38847fc50918f3dd520c3a
Nicolas Colombo <nicolas100533@gmail.com>
2023-08-03 12:37:11 -0300

    .

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -6,7 +6,7 @@ const AboutMe = () => {
     <div id='aboutMe' className={`${styles.container} flex flex-col text-xl justify-center items-center py-28 px-14 bg-white text-black`}>
       <p className='text-3xl font-light text-center'>UN POCO SOBRE MI</p>
       <figure className='w-44 h-w-44 my-6 rounded-full overflow-hidden'>
-        <img src="/images/aoba.jpg" alt="" />
+        <img src="/images/me.jpg" alt="" />
       </figure>
       <p className={`${styles.description} text-center text-lg font-normal mb-6`}>
         Hola! Mi nombre es Nicolas y soy un desarrollador web con mucha pasion por el desarrollo.
